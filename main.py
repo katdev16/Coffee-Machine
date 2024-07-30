5134ab11166a627d82de9d91a7a2dc6cba2e89ac
katdev16 <katlegodhlamini2003@gmail.com>
2024-07-30 19:28:45 +0200

    resources

--- main.py
+++ main.py
@@ -24,36 +24,71 @@ MENU = {
     }
 }
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
 
 def Coffee_Selection(Coffee):
+    Machine={
+    "water": 500,
+    "milk": 500,
+    "coffee": 500
+}
     if Coffee in MENU:
+        
+        water_amount = MENU[Coffee]["ingredients"]["water"]
+        milk_amount = MENU[Coffee]["ingredients"]["milk"]
+        coffee_amount = MENU[Coffee]["ingredients"]["coffee"]
+
+        Water_Level = Machine.get("water")
+        Milk_Level = Machine.get("milk")
+        Coffee_Level = Machine.get("coffee")
+
+        Water_Level-=water_amount
+        print(Water_Level)
+        milk_amount = Milk_Level-milk_amount
+        coffee_amount = Coffee_Level-coffee_amount
+
+        if Water_Level < 0 or Milk_Level < 0  or Coffee_Level < 0:
+            print("Sorry there is not enough water.") 
+            
+        
         return f"Your selection: {Coffee}"
     else:
         return "Unavailable"
+    
+
 User_input = input("")
+
+
 def Off():
     if User_input == "off":
         return False
     return True
 
+
 def report():
     if User_input == "report":
         return MENU.get("espresso")
-    
 
-while Off():
-    User_input = input("What would you like? (espresso/latte/cappuccino):")
-    while User_input == "report":
-        print(report())
+Machine={
+    "water": 500,
+    "milk": 500,
+    "coffee": 500
+}
+
+
+if __name__ == "__main__":
+   
+    print("Cofee Machine")
+    print(Machine.get("water"))
+    print(MENU["latte"]["ingredients"]["water"])
+
+    while Off():
         User_input = input("What would you like? (espresso/latte/cappuccino):")
+        while User_input == "report":
+            print(report())
+            User_input = input("What would you like? (espresso/latte/cappuccino):")
 
-    
-    print(Coffee_Selection(User_input))
+        
+        print(Coffee_Selection(User_input))
+        print(Machine.get("water"))
 
 
