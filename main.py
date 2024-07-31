742bd32ec43ce79b190037386efdee7e09f97e1b
katdev16 <katlegodhlamini2003@gmail.com>
2024-07-31 14:30:26 +0200

    process_coins works

--- main.py
+++ main.py
@@ -42,22 +42,24 @@ def Coffee_Selection(Coffee):
 
     if Coffee in MENU:
         
-        
         water_amount = MENU[Coffee]["ingredients"]["water"]
         milk_amount = MENU[Coffee]["ingredients"]["milk"]
         coffee_amount = MENU[Coffee]["ingredients"]["coffee"]
 
         Water_Level-=water_amount
+        Coffee_Level-=coffee_amount
         print(f"Water level: {Water_Level}")
 
 
         if Water_Level < 0 or Milk_Level < 0  or Coffee_Level < 0:
-            return "Sorry there is not enough water."
-            
-        
-        return f"Your selection: {Coffee}"
+            print("Sorry there is not enough water.")
+            return False
+        print(f"Your selection: {Coffee}")
+        return True
     else:
-        return "Unavailable"
+        print("Unavailable")
+        return False
+    
     
 
 
@@ -73,10 +75,36 @@ def Off():
 def report():
     if User_input == "report":
         return MENU.get("espresso")
+    
+def process_coins():
+    quarter = 0.25
+    dime = 0.10
+    nickle = 0.05
+    pennie = 0.01
+    coins = 0
+
+    User_input = input("Insert coins(quarter/dime/ nickle/pennie): ")
+    while User_input not in ["quarter","dime","nickle","pennie"]:
+        print("1st")
+        User_input = input("Insert coins(quarter/dime/ nickle/pennie): ")
+        coins+=quarter
+        print(coins)
+    
+    cost_coffee = MENU["latte"]["cost"]
+    print(cost_coffee)
+    while cost_coffee > coins:
+        print("2nd")
+        
+        User_input = input("Insert coins(quarter/dime/ nickle/pennie): ")
+        coins+=quarter
+        print(coins)
+    return "added"
+
+    
+
 
 
 if __name__ == "__main__":
-  
     Water_Level = Machine.get("water")
     water_amount = MENU["latte"]["ingredients"]["water"]
     print(water_amount)
@@ -90,9 +118,9 @@ if __name__ == "__main__":
         while User_input == "report":
             print(report())
             User_input = input("What would you like? (espresso/latte/cappuccino):")
-
+        Coffee_Selection(User_input)
         
-        print(Coffee_Selection(User_input))
+        process_coins()
 
 
 
